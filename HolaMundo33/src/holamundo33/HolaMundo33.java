49fce979f843921f3cd5047ea13d4478afbc2d45
Miguel Ortiz <elsuperm2016@gmail.com>
2023-02-26 10:18:56 -0500

    Ejercicios de Java de Mision TIC y practica de sintaxis

---
+++ HolaMundo33/src/holamundo33/HolaMundo33.java
@@ -0,0 +1,21 @@
+/*
+* programa de hola mundo
+ */
+package holamundo33;
+
+/**
+ *
+ * @author migue
+ */
+public class HolaMundo33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String mensaje="Hola Mundo Grupo 33";
+        System.out.println(mensaje);
+    }
+    
+}
