3242154b5eaa202dd95bf8b1d4fa56357f22af8f
Miguel Ortiz <elsuperm2016@gmail.com>
2023-10-22 11:56:09 -0500

    cambios practica

--- practica/src/practica.java
+++ practica/src/practica.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class practica {
+import java.util.Scanner;
+
+public class practica {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Digite un numero entero: ");
+        numero = sc.nextInt();
+        if (numero > 0){
+            System.out.print(numero + " es positivo y ");
+        }else{
+            System.out.print(numero + " es negativo y ");
+        }
+        if (numero % 2 == 0){
+            System.out.print("es par ");
+        }else{
+            System.out.print("es impar ");
+        }
+
+
+    }
+
+
+
 }
+
