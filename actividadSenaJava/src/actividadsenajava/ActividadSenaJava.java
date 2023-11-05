e1051b59310f4a714d9bed059cf101608968afe4
Miguel Ortiz <elsuperm2016@gmail.com>
2023-11-05 11:22:50 -0500

    ejercicios de jerarquia de variables

--- actividadSenaJava/src/actividadsenajava/ActividadSenaJava.java
+++ actividadSenaJava/src/actividadsenajava/ActividadSenaJava.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package actividadsenajava;
 
 /**
@@ -14,7 +10,42 @@ public class ActividadSenaJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba ");
+
+        double a = 2.37;
+        double b = 13.25;
+        double c = 1.43;
+        double d = 1.1;
+
+        double resultado;
+
+        resultado = (4 * a) + (5 * b) + c - d * (3 * a * b * c) - a + b + c;
+        System.out.println(resultado);
+
+        resultado = 9 / 3 * 12 + 16 - 8;
+        System.out.println(resultado);
+
+        resultado = 2 * 4 + 2.5 - 1.5 * 3 + 2 * 1 / 2.5;
+        System.out.println(resultado);
+
+        resultado = 5 * 7 + 3 / 2 + 4;
+        System.out.println(resultado);
+
+        double aa = 2;
+        double bb = 1.5;
+        double cc = 3.7;
+
+        resultado = 2 * aa + bb - aa + bb + cc + 5 * cc;
+        System.out.println(resultado);
+
+        resultado = 35 - 2 * 6 / 2 - 4 + 2 + 1.5 - 6 * 5 * 2;
+        System.out.println(resultado);
+
+        resultado = 7*5-2+32+8/1.6+1.5*0.8;
+        System.out.println(resultado);
+
+        resultado = 5*7+3/2+4;
+        System.out.println(resultado);
+
     }
 
 }
